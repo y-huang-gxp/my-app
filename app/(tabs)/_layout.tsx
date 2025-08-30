7d45c20d6468a70ca1b74930bf3587b5c6fd94f1
y.huang <y.huang@gxp.co.jp>
2025-08-30 23:22:51 +0900

    init

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,10 @@
-import { Tabs } from 'expo-router';
-import React from 'react';
-import { Platform } from 'react-native';
-
-import { HapticTab } from '@/components/HapticTab';
+import { HapticTab } from '@/components/ui/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
 import TabBarBackground from '@/components/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
+import { Tabs } from 'expo-router';
+import { Platform } from 'react-native';
 
 export default function TabLayout() {
   const colorScheme = useColorScheme();
@@ -21,7 +19,7 @@ export default function TabLayout() {
         tabBarStyle: Platform.select({
           ios: {
             // Use a transparent background on iOS to show the blur effect
-            position: 'absolute',
+           
           },
           default: {},
         }),
--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,9 +3,11 @@ import { Text } from '@/components/ui/text';
 
 export default function HomeScreen() {
  return (
-    <Center className="bg-primary-500 h-[150px] w-[250px]">
+  
+<Center className="bg-primary-500 h-[150px] w-[250px] mx-auto my-auto flex justify-center items-center">
       <Text className="text-typography-0 font-bold">This is the center.</Text>
     </Center>
+ 
   );
 }
 
